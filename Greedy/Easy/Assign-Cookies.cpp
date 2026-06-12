181b8f38e86c151a067918cea7c69fcafc88aade
Meghana-Merla <24je0654@iitism.ac.in>
2026-06-12 17:34:02 +0530

    Solved Assign Cookies

---
+++ Greedy/Easy/Assign-Cookies.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(s.begin(),s.end());
+        sort(g.begin(),g.end());
+        int i=0,j=0;
+        int ans=0;
+        while(i<g.size()&&j<s.size()){
+            if(g[i]<=s[j]){
+                i++;
+                j++;
+                ans++;
+            }else j++;
+        }return ans;
+    }
+};
