0b24901637b3a63d0fe876a2ae842c7bfe86e2e1
Meghana-Merla <24je0654@iitism.ac.in>
2026-06-13 13:24:52 +0530

    Solved Check if tree is BST or not

---
+++ Binary-Search-Trees/Practice/Check-if-tree-is-BST-or-not.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    class TreeNode {
+    public:
+        int val;
+        TreeNode *left;
+        TreeNode *right;
+        TreeNode() : val(0), left(nullptr), right(nullptr) {}
+        TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    };
+    bool isValidBST(TreeNode* root) {
+        return isValidBSTUtil(root, LONG_MIN, LONG_MAX);
+    }
+    
+    bool isValidBSTUtil(TreeNode* root, long minVal, long maxVal) {
+        if (root == nullptr) {
+            return true;
+        }
+        
+        if (root->val <= minVal || root->val >= maxVal) {
+            return false;
+        }
+        
+        return isValidBSTUtil(root->left, minVal, root->val) &&
+               isValidBSTUtil(root->right, root->val, maxVal);
+    }
+};
