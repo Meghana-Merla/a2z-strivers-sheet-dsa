cf69649634f0abee22637fb14329c28e04fa4564
Meghana-Merla <24je0654@iitism.ac.in>
2026-06-13 17:26:48 +0530

    Solved Insert into BST

---
+++ Binary-Search-Trees/Practice/Insert-into-BST.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    class TreeNode {
+    public:
+        int val;
+        TreeNode *left;
+        TreeNode *right;
+        TreeNode() : val(0), left(nullptr), right(nullptr) {}
+        TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    };
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(root==NULL){
+            return new TreeNode(val);
+        }
+        if(val>root->val) root->right=insertIntoBST(root->right,val);
+        else root->left=insertIntoBST(root->left,val);
+        return root;
+    }
+};
