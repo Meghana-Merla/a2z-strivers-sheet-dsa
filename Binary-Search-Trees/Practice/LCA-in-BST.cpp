768708bbcfc0025882e18d714234c8a57e9e4855
Meghana-Merla <24je0654@iitism.ac.in>
2026-06-13 17:19:54 +0530

    Solved Lowest Common Ancestor in BST

---
+++ Binary-Search-Trees/Practice/LCA-in-BST.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    class TreeNode {
+    public:
+        int val;
+        TreeNode *left;
+        TreeNode *right;
+        TreeNode() : val(0), left(nullptr), right(nullptr) {}
+        TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    };
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root==NULL) return NULL;
+        if(root->val>p->val && root->val>q->val) return lowestCommonAncestor(root->left,p,q);
+        else if(root->val<p->val && root->val<q->val) return lowestCommonAncestor(root->right,p,q);
+        else return root;
+    }
+};
