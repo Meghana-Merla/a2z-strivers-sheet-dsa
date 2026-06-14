42ecfdc8e247289f6c1ae70bb44236fb9355f84e
Meghana-Merla <24je0654@iitism.ac.in>
2026-06-14 12:46:02 +0530

    Solved Postorder Traversal

---
+++ Binary-Tree/Traversals/Postorder.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    class TreeNode {
+    public:
+        int val;
+        TreeNode *left;
+        TreeNode *right;
+        TreeNode() : val(0), left(nullptr), right(nullptr) {}
+        TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    };
+    vector<int>ans;
+    vector<int> postorderTraversal(TreeNode* root) {
+        help(root);
+        return ans;
+    }
+    void help(TreeNode* root){
+        if(!root) return;
+        help(root->left);
+        help(root->right);
+        ans.push_back(root->val);
+    }
+};
