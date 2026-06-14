07e316a929201a218f801ebc74429d308c57aa88
Meghana-Merla <24je0654@iitism.ac.in>
2026-06-14 15:32:48 +0530

    Solved Bottom View of Binary Tree

---
+++ Binary-Tree/Medium/Bottom-View.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<map>
+using namespace std;
+
+class Solution {
+public:
+    class TreeNode {
+    public:
+        int val;
+        TreeNode *left;
+        TreeNode *right;
+        TreeNode() : val(0), left(nullptr), right(nullptr) {}
+        TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    };
+    vector<int> bottomView(TreeNode* root) {
+        if(!root) return {};
+        vector<int>ans;
+        map<int,int>mp;
+        queue<pair<TreeNode*,int>>q;
+        q.push({root,0});
+        while(!q.empty()){
+            auto it=q.front();
+            q.pop();
+            TreeNode* node=it.first;
+            int hd=it.second;
+            mp[hd]=node->val;
+            if(node->left) q.push({node->left,hd-1});
+            if(node->right) q.push({node->right,hd+1});
+        }
+        for(auto it:mp){
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
