8d78bb2342d41114709613c68657ce2a1a14c529
Meghana-Merla <24je0654@iitism.ac.in>
2026-06-16 01:02:07 +0530

    Solved Number of times array is rotated

---
+++ Binary-Search/1D/No.of-times-rotated.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int countRotations(vector<int>&nums){
+        int low=0,high=nums.size()-1;
+        while(low<high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]>nums[high]) low=mid+1;
+            else high=mid;
+        }
+        return low;
+    }
+};
